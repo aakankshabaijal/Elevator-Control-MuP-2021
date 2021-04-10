fb80b2555efa7f336065a8ca420ec2600daaf489
Rohit-Mundada <rohitrm0801@gmail.com>
2021-04-10 18:31:31 +0530

    Revised Keyboard ISR

--- keyboard_isr.asm
+++ keyboard_isr.asm
@@ -20,17 +20,44 @@
 
 ; base address of 8255 used is 00h
 
+; base address of 8253 used is 80h
+
 ; not writing the debounce logic assuming that the button press is genuine
 
-.data
+#make_bin#
 
-table_k db 0eeh, 0edh, 0ebh, 0e7h, 0deh, 0ddh, 0dbh, 0d7h, 0beh, 0bdh, 0bbh, 0b7h, 7eh, 7dh
+#LOAD_SEGMENT=FFFFh#
+#LOAD_OFFSET=0000h#
 
-btn_press db ?
+#CS=0000h#
+#IP=0000h#
 
-.code 
-.startup
-    mov al, 10011000b
+#DS=0000h#
+#ES=0000h#
+
+#SS=0000h#
+#SP=FFFEh#
+
+#AX=0000h#
+#BX=0000h#
+#CX=0000h#
+#DX=0000h#
+#SI=0000h#
+#DI=0000h#
+#BP=0000h#
+
+jmp st1
+
+; table db 0eeh, 0edh, 0ebh, 0e7h, 0deh, 0ddh, 0dbh, 0d7h, 0beh, 0bdh, 0bbh, 0b7h, 7eh, 7dh
+
+liftMove db 0
+floorNum db 0
+direction db 0
+doorState db 0
+
+st1: 
+    cli
+    mov al, 10011000b ;writing into 8255 control word
     out 06h, al
 
     ; check for key press
@@ -79,18 +106,103 @@ x1: in al, 04h
 
     ; find out which key was pressed from hex keypad
 check_key: 
-    or al, bl
-    mov cx, 0dh ; setting cx = 0014d so that we can loop through all 14 elements of table_k
-    mov di, 00h
-    ;find out which element of table_k match with al
-x3: cmp al, table_k[di]
-    jz x2
-    inc di
-    loop x3
+    or al, bl ; which key is pressed
+    cmp al, 0eeh
+    jnz x3
+    call doorClose
+    jmp check_key
 
-x2: mov btn_press, al
+x3: cmp al, 0edh
+    jnz x4
+    call coarseSensor
+    jmp check_key
+
+x4: cmp al, 0ebh
+    jnz x5
+    call fineSensor
+    jmp check_key
+
+x5: cmp al, 0e7h
+    jnz x6
+    call coarseSensor
+    jmp check_key
+
+x6: cmp al, 0deh
+    jnz x7
+    call up0
+    jmp check_key
+
+x7: cmp al, 0ddh
+    jnz x8
+    call down1
+    jmp check_key
+
+x8: cmp al, 0dbh
+    jnz x9
+    call up1
+    jmp check_key
+
+x9: cmp al, 0d7h
+    jnz x10
+    call down2
+    jmp check_key
+
+x10:cmp al, 0beh
+    jnz x11
+    call up2
+    jmp check_key
+
+x11:cmp al, 0bdh
+    jnz x12
+    call down3
+    jmp check_key
+
+x12:cmp al, 0bbh
+    jnz x13
+    call lift0
+    jmp check_key
+
+x13:cmp al, 0b7h
+    jnz x14
+    call lift1
+    jmp check_key
+
+x14:cmp al, 07eh
+    jnz x15
+    call lift2
+    jmp check_key
+
+x15:cmp al, 07dh
+    jnz check_key
+    call lift3
+    jmp check_key
 
 
+    
+    
+;     mov cx, 14 ; setting cx = 0014d so that we can loop through all 14 elements of table
+;     mov di, 00h
+;     ;find out which element of table match with al
+; x3: cmp al, table[di]
+;     jz x2
+;     inc di
+;     loop x3
+
+; x2: cmp al, 0eeh
+;     jz doorClose
+;     cmp al, 0edh
+;     jz 
+
+
+
+
+
+
+
+
+
+;  D7  D6  D5  D4  D3  D2  D1  D0
+;                               
 
 
     
